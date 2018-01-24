97b9da33fcd585899de2df83cc593195c2ae580e
Martynas Bagdonas <martbgd@gmail.com>
2018-01-24 13:11:20 +0200

    Force LF EOL on all platforms. Added missing contributor

--- pdftotext.cc
+++ pdftotext.cc
@@ -25,6 +25,7 @@
 // Copyright (C) 2013 Yury G. Kudryashov <urkud.urkud@gmail.com>
 // Copyright (C) 2013 Suzuki Toshiya <mpsuzuki@hiroshima-u.ac.jp>
 // Copyright (C) 2015 Jeremy Echols <jechols@uoregon.edu>
+// Copyright (C) 2017 Adrian Johnson <ajohnson@redneon.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
@@ -330,6 +331,9 @@ int main(int argc, char *argv[]) {
 
     // read config file
     globalParams = new GlobalParams(datadir);
+
+    // force LF EOL on all platforms
+    globalParams->setTextEOL("unix");
 
     fileName = new GooString(argv[1]);
     if (fixedPitch) {
