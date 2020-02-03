23e1cfe4afdf74e7142c03e349c50a5ecffd055d
jmechnich <joerg.mechnich@gmail.com>
2020-02-03 11:30:58 +0100

    Updated Debian base to Buster. Simplified macOS SDK handling and updated README

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build . -t cross-poppler
-docker run -it -v  $(pwd)/build:/output cross-poppler /bin/bash -c "rm -rf /output/* && cp -r /build/pdftools.tar.gz /output/"
+docker build --rm -t cross-poppler .
+docker run --rm -it -v  $(pwd)/build:/output cross-poppler /bin/bash -c "rm -rf /output/* && cp -r /build/pdftools.tar.gz /output/"
